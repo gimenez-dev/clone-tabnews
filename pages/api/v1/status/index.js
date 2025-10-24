ec12dbc0edb8262c253a367f3ca22a656fd30459
gimenez-dev <gabrielgimenez1402@gmail.com>
2025-10-24 11:44:05 -0300

    feat: Create database and add .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+const status = async (request, response) => {
+  const result = await database.query("SELECT 1 + 1;");
   response.status(200).json({ status: "ok" });
-}
+};
 
 export default status;
